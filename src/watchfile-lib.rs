b97e4dbd34a1a70c789ef63e936f8ca8376bb369
richbl <richbl@gmail.com>
2024-03-21 20:34:40 -0700

    minor cleanup of file syntax

--- src/watchfile-lib.rs
+++ src/watchfile-lib.rs
@@ -15,6 +15,7 @@ pub fn load_toml_config<T: for<'de> serde::Deserialize<'de>>(path: &str) -> Resu
   //
   let toml_content = read_to_string(path).map_err(|err| format!("Failed to read TOML file: {}", err))?;
 
+  //
   // Parse TOML file into Config struct
   //
   let config = from_str(&toml_content).map_err(|err| format!("Failed to parse TOML file: {}", err))?;
